17aefac19f759a6610e84b5f0dbbcf5a37c01a5b
Verena Kopf <verena.kopf@gmx.net>
2018-04-27 13:38:48 +0200

    more files

---
+++ GameStore/GameStore/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore
+{
+    class Game
+    {
+        IState _State;
+
+        IState State
+        {
+            get
+            {
+                return _State;
+            }
+
+            set
+            {
+                _State = value;
+            }
+        }
+    }
+}
--- GameStore/GameStore/User.cs
+++ GameStore/GameStore/User.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GameStore
 {
-    interface Interface1
+    class User
     {
     }
 }
